3b6fec54cc048a7c6b0c13a13f36c21d28a126c6
CoSuggs <cody.m.suggs@gmail.com>
2022-08-30 09:07:51 -0700

    algos d2

---
+++ MERN/w1/d2/algos.js
@@ -0,0 +1,98 @@
+/* 
+  https://visualgo.net/en/sorting
+    
+  Selection sort works by iterating through the list, finding the minimum
+  value, and moving it to the beginning of the list. Then, ignoring the first
+  position, which is now sorted, iterate through the list again to find the
+  next minimum value and move it to index 1. This continues until all values
+  are sorted.
+  Unstable sort.
+  
+  Time Complexity
+    - Best: O(n^2) quadratic.
+    - Average: O(n^2) quadratic.
+    - Worst: O(n^2) quadratic.
+  Space: O(1) constant.
+  Selection sort is one of the slower sorts.
+  - ideal for: pagination, where page 1 displays 10 records for example,
+      you run selection sort for 10 iterations only to display the first 10
+      sorted items.
+*/
+
+const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Sorts given array in-place.
+ * Best: O(n^2) quadratic.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic.
+ * @param {Array<number>} nums
+ * @returns {Array<number>} The given array after being sorted.
+ */
+function selectionSort(nums = []) {
+    for (let i = 0; i < nums.length; i++) {
+        for(let j = 0; j<nums.length; j++){
+            if(nums[i]<nums[j]){
+                let temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+            }
+        }
+    }return nums
+}
+console.log(selectionSort(numsRandomOrder));
+console.log(selectionSort(numsReversed));
+/***************************************************************************************************** */
+/* 
+  - Visualization with playing cards (scroll down):
+      https://www.khanacademy.org/computing/computer-science/algorithms/insertion-sort/a/insertion-sort
+  - Array / bar visualization:
+      https://www.hackerearth.com/practice/algorithms/sorting/insertion-sort/visualize/
+  - Stable sort, efficient for small data sets or mostly sorted data sets.
+  Time Complexity
+    - Best: O(n) linear when array is already sorted.
+    - Average: O(n^2) quadratic.
+    - Worst: O(n^2) quadratic when the array is reverse sorted.
+  Space: O(1) constant.
+  - this sort splits the array into two portions (conceptually, not literally).
+  - the left portion will become sorted, the right portion
+      (that hasn't been iterated over yet) is unsorted.
+  
+*/
+
+// const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+// const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Sorts the given array in-place.
+ * Best: O(n) linear when array is already sorted.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic when the array is reverse sorted.
+ * @param {Array<number>} nums
+ * @returns {Array<number>} The given array after being sorted.
+ * PSUEDO Code
+ * consider the first item in any list as sorted
+ * starting at of index one check if val is less than val at index to the left
+ *  -if yes, swap value
+ *  - repeat until val is in correct position
+ * move on to next index
+ * return when finsished
+ */
+function insertionSort(nums = []) {
+    for (let i = 0; i < nums.length; i++) {
+        for(let j = 0; j<nums.length; j++){
+            if(nums[j]>nums[j+1]){
+                let temp = nums[j+1]
+                nums[j+1] = nums[j]
+                nums[j] = temp
+            }
+        }
+    }return nums
+}
+console.log(insertionSort(numsRandomOrder));
+console.log(insertionSort(numsReversed));
