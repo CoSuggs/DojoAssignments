5c928ee3e4cc6932f044f80acc70a9d5fda7a6d1
CoSuggs <cody.m.suggs@gmail.com>
2022-09-14 13:52:21 -0700

    Weird things afoot, debugging

--- MERN/react/axiosPokemon/axios-pokemon/src/App.js
+++ MERN/react/axiosPokemon/axios-pokemon/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import App from './App';
 
-function App() {
+const someComponent = props => {
+  //Note the second argument is an empty array.
+  const [responseData, setResponseData] = useState(null);
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/')
+      .then(response => { setResponseData(response.data) })
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {responseData}
     </div>
-  );
+  )
 }
 
-export default App;
