38631334e7f8b628e8a66a4d4ed5b3129e4a1cac
CoSuggs <cody.m.suggs@gmail.com>
2022-09-19 09:12:04 -0700

    new things added

--- MERN/react/luke-api/src/App.js
+++ MERN/react/luke-api/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  function fetchPokemon() {
+    axios.get('https://swapi.dev/api/people?limit=80')
+      .then(response => {
+        console.log(response.data.results);
+        setPokemon(response.data.results);
+      })
+      .catch(error => console.log(error));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}  >
+      <div>
+        <button onClick={fetchPokemon}>Fetch All Pokemon</button>
+      </div>
+      <hr />
+      <div>
+        {pokemon.map(pokemon => (
+          <p key={ pokemon.name }>{ pokemon.name }</p>
+        ))}
+      </div>
+
     </div>
   );
 }
