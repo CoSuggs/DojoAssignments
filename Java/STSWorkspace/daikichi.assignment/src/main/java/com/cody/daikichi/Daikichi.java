d571522b16e91c6f333d2485c15763a29cacf09c
CoSuggs <cody.m.suggs@gmail.com>
2022-10-04 08:02:18 -0700

    new items

---
+++ Java/STSWorkspace/daikichi.assignment/src/main/java/com/cody/daikichi/Daikichi.java
@@ -0,0 +1,11 @@
+package com.cody.daikichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daikichi {
+	public static void main(String[] args) {
+		SpringApplication.run(Daikichi.class, args);
+	}
+}
