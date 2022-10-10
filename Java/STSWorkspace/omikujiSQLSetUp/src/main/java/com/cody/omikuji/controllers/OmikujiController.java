780e938818e7da4790cc1e5110365a5bcb5014d8
CoSuggs <cody.m.suggs@gmail.com>
2022-10-10 08:05:32 -0700

    new assignment

--- Java/STSWorkspace/omikujiSQLSetUp/src/main/java/com/cody/omikuji/controllers/OmikujiController.java
+++ Java/STSWorkspace/omikujiSQLSetUp/src/main/java/com/cody/omikuji/controllers/OmikujiController.java
@@ -12,28 +12,25 @@ import com.cody.omikuji.models.OmikujiModel;
 
 @Controller
 public class OmikujiController {
-	
+
 	@RequestMapping("/")
 	public String index() {
 		return "index.jsp";
 	}
-	
-	@RequestMapping(value="/omikuji", method=RequestMethod.POST)
-	public String omikuji(
-			@RequestParam(value="num") Integer num,
-			@RequestParam(value="cityName") String cityName,
-			@RequestParam(value="realName") String realName,
-			@RequestParam(value="hobby") String hobby,
-			@RequestParam(value="livingThing") String livingThing,
-			@RequestParam(value="comment") String comment) {
+
+	@RequestMapping(value = "/omikuji", method = RequestMethod.POST)
+	public String omikuji(@RequestParam(value = "num") Integer num, @RequestParam(value = "cityName") String cityName,
+			@RequestParam(value = "realName") String realName, @RequestParam(value = "hobby") String hobby,
+			@RequestParam(value = "livingThing") String livingThing, @RequestParam(value = "comment") String comment) {
 		return "redirect:/omikuji/show";
 	}
-	
-	@RequestMapping(value="/omikuji/show")
+
+	@RequestMapping(value = "/omikuji/show")
 	public String showOmikuji(Model model) {
 		ArrayList<OmikujiModel> omikuji = new ArrayList<OmikujiModel>();
 		omikuji.add(new OmikujiModel(10, "Seattle", "Robert Frost", "Programming", "Dog", "I really hope this works"));
-		omikuji.add(new OmikujiModel(20, "Portland", "Robert Denero", "Engineer", "Tiger", "I really hope this works as well or this was all for not."));
+		omikuji.add(new OmikujiModel(20, "Portland", "Robert Denero", "Engineer", "Tiger",
+				"I really hope this works as well or this was all for not."));
 		model.addAttribute("omikujiFromController", omikuji);
 		return "OmikujiShow.jsp";
 	}
